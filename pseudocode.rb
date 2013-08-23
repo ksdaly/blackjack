2eb1e07766c3e91889e0ca327a1c7f02e49c49fd
Kate Daly <kate.s.daly@gmail.com>
2013-08-23 17:27:25 -0400

    added pseudocode

---
+++ pseudocode.rb
@@ -0,0 +1,23 @@
+# Deal out cards; show both of the player's cards and one of the dealer's cards
+# Determine all possible scores for player's cards
+# If (One of the scores is 21)
+#     Goto Dealer's Turn  (this is a blackjack)
+# Else
+#     While (Hitting)
+#         Add one card to player's hand
+#         Determine all possible scores for player's cards
+#         If minimum score > 21
+#             Goto Dealer's Turn  (this is a bust)
+#         End If
+#     End While
+# End If
+
+# Dealer's Turn
+#     Determine highest score for player that's 21 or lower
+#     Play Out Dealer's Hand
+#     Determine Winner
+
+
+
+
+
