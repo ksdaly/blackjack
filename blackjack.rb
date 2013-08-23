1787738f908284a137c249d79900b8ced321ff3f
Kate Daly <kate.s.daly@gmail.com>
2013-08-23 16:50:05 -0400

    successful refactoring

--- blackjack.rb
+++ blackjack.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
-# require 'pry'
+require 'pry'
 
 SUITS = ['♠', '♣', '♥', '♦']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -44,11 +44,14 @@ def calculate_value(cards)
   if value_array.count(ACE) >= 1 && bust(total)
     total -= 10
   end
+
   return total
 end
 
-def show_card(player, card)
-  "#{player} was dealt: #{card}"
+def show_card(player, cards)
+  cards.each do |card|
+    puts "#{player} was dealt: #{card}"
+  end
 end
 
 def show_total(player, total)
@@ -99,36 +102,51 @@ def credit_card_scam
   end
 end
 
-puts "Welcome to B * L * A * C * K * J * A * C * K"
+def say_hello
+  "Welcome to B * L * A * C * K * J * A * C * K"
+end
+
+def deal_cards(user_hands, cards)
+  cards.times do
+    user_hands.each do |hand|
+      hand << @deck.pop
+    end
+  end
+end
+
+def play_turn(user, user_hands)
+  deal_cards([user_hands], 1)
+  show_card(user, [user_hands[-1]])
+  total = calculate_value(user_hands)
+  puts show_total(user, total)
+  total
+end
 
 
-correct_input = ["h","s"]
+puts say_hello
+
+
+@correct_input = ["h","s"]
 
 player = "Player"
 dealer = "Dealer"
 
-player_cards = []
-dealer_cards = []
+@player_cards = []
+@dealer_cards = []
 
-deck = build_deck
+@deck = build_deck
 
 # credit_card_scam
 
-2.times do
+deal_cards([@player_cards, @dealer_cards], 2)
 
-  # player_cards << deck.pop
-  dealer_cards << deck.pop
-end
-player_cards << 'A♠'
-player_cards << 'K♠'
-player_total = calculate_value(player_cards)
-dealer_total = calculate_value(dealer_cards)
+@player_total = calculate_value(@player_cards)
+@dealer_total = calculate_value(@dealer_cards)
 
-puts show_card(player, player_cards[0])
-puts show_card(player, player_cards[1])
-puts show_total(player, player_total)
+show_card(player, @player_cards)
 
-if game_play(player_total, 21)
+puts show_total(player, @player_total)
+
 
 
 next_turn = nil
@@ -137,37 +155,29 @@ while !stay(next_turn)
   next_turn = prompt("[h]it or [s]tay?")
 
   case
-    when !correct_input.include?(next_turn)
+    when !@correct_input.include?(next_turn)
       puts "Invalid input, please put either 'h' for hit or 's' for stay."
 
     when hit(next_turn)
-      new_card = deck.pop
-      player_cards << new_card
-      puts show_card(player, player_cards[-1])
-      player_total = calculate_value(player_cards)
-      puts show_total(player, player_total)
 
-      if bust(player_total)
+      @player_total = play_turn(player, @player_cards)
+
+      if bust(@player_total)
         puts "Player busts, dealer wins."
         break
       end
 
     when stay(next_turn)
-      player_total = calculate_value(player_cards)
-      show_total(player, player_total)
+      player_total = calculate_value(@player_cards)
+      show_total(player, @player_total)
       puts "Dealer's turn."
-      puts show_card(dealer, dealer_cards[0])
-      puts show_card(dealer, dealer_cards[1])
-      puts show_total(dealer, dealer_total)
+      show_card(dealer, @dealer_cards)
+      puts show_total(dealer, @dealer_total)
 
-      while game_play(dealer_total, 17)
-        new_card = deck.pop
-        dealer_cards << new_card
-        puts show_card(dealer, dealer_cards[-1])
-        dealer_total = calculate_value(dealer_cards)
-        puts show_total(dealer, dealer_total)
+      while game_play(@dealer_total, 17)
+        @dealer_total = play_turn(dealer, @dealer_cards)
       end
 
-      scoring(player_total, dealer_total)
+      scoring(@player_total, @dealer_total)
   end
 end
