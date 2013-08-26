7921bb21f8b3bffdbb331bd749031df0c6ead37f
Kate Daly <kate.s.daly@gmail.com>
2013-08-26 17:07:34 -0400

    working on Deck class

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -1,7 +1,28 @@
+#!/usr/bin/env ruby
+require 'pry'
+
 class Deck
+  attr_accessor :deck
+
+  def initialize
+    @deck = []
+    build_deck
+    @deck.shuffle!
+  end
+
+  def build_deck
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        @deck.push(value + suit)
+      end
+    end
+  end
+
   def pop
+    @deck.pop
   end
 end
+
 
 class Card
 end
@@ -19,3 +40,12 @@ class Hand
   def busted?
   end
 end
+
+SUITS = ['♠', '♣', '♥', '♦']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+puts original_deck = Deck.new
+puts original_deck.inspect
+puts original_deck.pop
+puts original_deck.inspect
+
