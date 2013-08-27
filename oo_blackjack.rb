10fb1dea6c3c90e8413a725b5af3338779504b59
kdurr <kristina.l.durr@gmail.com>
2013-08-27 17:11:40 -0400

    after help request

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -2,65 +2,104 @@
 require 'pry'
 
 class Deck
-  attr_accessor :deck
+  attr_accessor :cards
+  SUITS = ['♠', '♣', '♥', '♦']
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
   def initialize
-    @deck = []
+    @cards = []
     build_deck
-    @deck.shuffle!
   end
 
   def build_deck
     SUITS.each do |suit|
       VALUES.each do |value|
-        @deck.push(value + suit)
+        @cards.push(Card.new(value, suit))
       end
     end
   end
 
-  def pop
-    @deck.pop
+  def next_card
+    @cards.pop
   end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
 end
 
 
 class Card
-  def initialize(deck)
-    @card = deck.pop
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
   end
 
-  def card_value
-    @card.chop
+  def value
+    if ['J', 'Q', 'K'].include?(@value)
+      10
+    elsif ['A'].include?(@value)
+      1
+    else
+      @value.to_i
+    end
   end
+
+  def suit
+    @suit
+  end
+
 end
 
 class Hand
-  def initialize
-
+  def initialize(name)
+    @name = name
+    @cards = []
   end
 
   def hit(card)
+    @cards << card
   end
 
   def stay
   end
 
   def score
+    puts @cards.map(&:value)
+    score = 0
+    @cards.each do |card|
+      score += card.value
+    end
+    if @cards.map(&:value).include?(1) && score + 10 <= 21
+      score +10
+    else
+      score
+    end
+
   end
 
   def busted?
   end
 end
 
+class Game
+  def initialize
+    @deck = Deck.new
+  end
+
+  def deal_hand
+    @deck.shuffle
+    @player_hand = Hand.new('player')
+    @dealer_hand = Hand.new('dealer')
+    @player_hand.hit(@deck.next_card)
+    @player_hand.hit(@deck.next_card)
+    puts @player_hand.score
+  end
+end
 
 
-SUITS = ['♠', '♣', '♥', '♦']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
-puts original_deck = Deck.new
-puts original_deck.inspect
-puts next_card = Card.new(original_deck)
-puts next_card.card_value
-
-puts original_deck.inspect
+game = Game.new
+game.deal_hand
 
